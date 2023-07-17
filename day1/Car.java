b422a82871a6de50dfde2c02dc3f579df969966d
sudhapriyapv <139778885+sudhapriyapv@users.noreply.github.com>
2023-07-17 19:14:38 +0530

    Add files via upload

---
+++ day1/Car.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class Car {
+
+	public static void main(String[] args) {
+		
+   int price = 1000000;
+   
+   String carBrandName = "Tata";
+   
+   char logo = 'T';
+   
+   float fuelQuantity = 25.20f;
+   
+   double model = 452.35687912;
+   
+   boolean isNew = true;
+   
+   long phoneNumber=958544908;
+   
+   System.out.println(carBrandName + " With logo " +logo +" has " +fuelQuantity +" and " +model +" XZ+ ");
+   System.out.println("My car is " + isNew);
+   
+   
+
+	}
+
+}
---
+++ day1/FireFox.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class FireFox {
+
+	public static void main(String[] args) {
+		
+		float version = 100.2f;
+		
+		String name = "firefox";
+		
+		boolean visible = true;
+		
+		int year = 1988;
+		
+		char logo = 'm';
+		
+		System.out.println("In my machine, I am having "+name +" with Logo as " +logo +" and Version as " +version);
+		
+		System.out.println("Visibility is " +visible);
+		
+		System.out.println("Year as " +year);
+
+	}
+
+}
